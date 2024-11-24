c6f074ba9176bca7f86f5710ed45cb455930c22e
Mick Lawitzke <info@lawitzke.de>
2024-11-24 15:24:11 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+onMounted(() => {
+  setTimeout(() => {
+    useTrackEvent('hello');
+  }, 1000);
+});
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer/>
+    <NuxtWelcome/>
+  </div>
+</template>
